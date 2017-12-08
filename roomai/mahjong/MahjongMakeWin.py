a412ede52403b60c590be70f595a91084e1e9fa4
jessyji(纪超) <jessyji@tencent.com>
2017-12-08 12:46:00 +0800

    update

--- roomai/mahjong/MahjongMakeWin.py
+++ roomai/mahjong/MahjongMakeWin.py
@@ -2,6 +2,29 @@ from MahjongUtil import MahjongCard
 
 suit_list = ['Dot','Bamboo','Character','EastWind','SouthWind','WestWind','NorthWind','Red','Green','WhiteDragon']
 point_list = ['1','2','3','4','5','6','7','8','9']
+
+def process_fun(card_list,key):
+    if len(card_list) == 13:
+        all_list.append(card_list)
+        return 
+    if mahjong_map[key] > 4:
+        return
+    if mahjong_map[key] <= 1:
+        card_list.append(key)
+        card_list.append(key)
+        card_list.append(key)
+        mahjong_map[key] = mahjong_map[key] + 3
+        for card in mahjong_map.key():
+            process_fun(card_list,card)
+    suit = key.split("_")[0]
+    point = key.split("_")[1]
+    if suit in ['EastWind','SouthWind','WestWind','NorthWind','Red','Green','WhiteDragon']:
+        return
+    if mahjong_map[suit + "_%s" % str(point + 1)] <= 3 and mahjong_map[suit + "_%s" % str(point + 2)] <= 3:
+        card_list.append(key)
+        card_list.append(suit + "_%s" % str(point + 1))
+        card_list.append(suit + "_%s" % str(point + 2))
+        
 if __name__ == '__main__':
     mahjong_map = {}
     for suit in suit_list:
@@ -11,6 +34,17 @@ if __name__ == '__main__':
         else:
             mahjong_map[suit + "_" + point] = 0
     print mahjong_map
+    all_list = []
+    for each in mahjong_map.keys():
+        cards_list = []
+        cards_list.append[each]
+        cards_list.append[each]
+        mahjong_map[each]  = mahjong_map[each] + 2
+        for each_1 in mahjong_map.keys():
+            if mahjong_map[each_1] > 4:
+                continue
+            else:
+
 
     #abc = dict(suit_map)
     #all_cards = {}
